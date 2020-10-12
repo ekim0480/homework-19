ac5a37b8a078f70f6bbcd3caff704637c05ae6a8
EK <ekim0480@gmail.com>
2020-10-12 15:00:00 -0500

    Updates

---
+++ precache-manifest.9848323096f838908f2e2b2032cc1761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2680189cd201f2aa907b3b86b117b823",
+    "url": "/homework-19/index.html"
+  },
+  {
+    "revision": "f44302daba91170a5aed",
+    "url": "/homework-19/static/css/main.06d82383.chunk.css"
+  },
+  {
+    "revision": "df1014c2d2e52464dea6",
+    "url": "/homework-19/static/js/2.a0e5a819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework-19/static/js/2.a0e5a819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44302daba91170a5aed",
+    "url": "/homework-19/static/js/main.b5654640.chunk.js"
+  },
+  {
+    "revision": "ad1be441d05e2ffd242f",
+    "url": "/homework-19/static/js/runtime-main.9d783eb5.js"
+  }
+]);
